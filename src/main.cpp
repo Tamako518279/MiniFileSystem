d59aa7f377a0e098942087f8620daa37995be515
Tamako <3271275805@qq.com>
2026-09-19 14:38:31 +0800

    Add command input loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 int main()
 {
-    std::cout << "MinFileSystem v0.1";
-    return 0;
+   std::string command;
+   std::cout << "请输入：";
+   while(1)
+   {
+     std::cout << "/>";
+     if(!std::getline(std::cin,command))
+     break;
+     if(command == "exit")
+     break;
+     else
+     std::cout << "Command: " << command << std::endl;
+   }
+   return 0;
+
 }
